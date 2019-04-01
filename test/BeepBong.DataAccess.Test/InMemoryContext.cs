05c51dcdab6db8b67d49423338d0db0b00a47547
Jimmyson <jimmyson.2007@gmail.com>
2019-04-01 21:55:24 +1100

    Add DataContext testing Test Shadow Properties creation

---
+++ test/BeepBong.DataAccess.Test/InMemoryContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.EntityFrameworkCore;
+using BeepBong.DataAccess;
+
+namespace BeepBong.DataAccess.Test
+{
+    public static class InMemoryContext
+    {
+        public static DbContextOptions<BeepBongContext> ContextGenerator(string name) {
+			return new DbContextOptionsBuilder<BeepBongContext>()
+				.UseInMemoryDatabase(databaseName: name)
+				.Options;
+		}
+    }
+}
---
+++ test/BeepBong.DataAccess.Test/ShadowPropertiesTest.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using Xunit;
+using BeepBong.Domain.Models;
+using BeepBong.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeepBong.DataAccess.Test
+{
+    public class ShadowPropertiesTest
+    {
+        [Fact]
+		public void CreateRecordTest()
+		{
+			var options = InMemoryContext.ContextGenerator("CreateRecordTest");
+
+			using (var context = new BeepBongContext(options)) {
+				context.Programmes.Add(new Programme()
+				{
+					Name = "Hello World",
+					Year = "2000"
+				});
+				context.SaveChanges();
+			};
+
+			using (var context = new BeepBongContext(options)) {
+				var result = context.Programmes.OrderBy(p => EF.Property<DateTime>(p, "Created")).ToList();
+
+				Assert.Single(result);
+
+				var createdValue = context.Entry(result.First()).Property("Created").CurrentValue;
+				var lastModifiedValue = context.Entry(result.First()).Property("LastModified").CurrentValue;
+
+				Assert.NotNull(createdValue);
+				Assert.Null(lastModifiedValue);
+			}
+		}
+
+		
+        [Fact]
+		public void UpdateRecordTest()
+		{
+			var options = InMemoryContext.ContextGenerator("UpdateRecordTest");
+
+			using (var context = new BeepBongContext(options)) {
+				context.Programmes.Add(new Programme()
+				{
+					Name = "Hello World",
+					Year = "2000"
+				});
+				context.SaveChanges();
+			};
+
+			using (var context = new BeepBongContext(options)) {
+				var p = context.Programmes.First();
+
+				p.Channel = "Sample Channel";
+
+				context.SaveChanges();
+			};
+
+			using (var context = new BeepBongContext(options)) {
+				var result = context.Programmes.OrderBy(p => EF.Property<DateTime>(p, "Created")).ToList();
+
+				Assert.Single(result);
+
+				var createdValue = context.Entry(result.First()).Property("Created").CurrentValue;
+				var lastModifiedValue = context.Entry(result.First()).Property("LastModified").CurrentValue;
+
+				Assert.NotNull(createdValue);
+				Assert.NotNull(lastModifiedValue);
+			}
+		}
+    }
+}
