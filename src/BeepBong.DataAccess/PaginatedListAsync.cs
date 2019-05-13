f0007de27102596d9e01dc3ed519dd27847210f9
Jimmyson <jimmyson.2007@gmail.com>
2019-05-13 21:44:08 +1000

    Add Pagination to Rendering Fix page index and size variable swapping

--- src/BeepBong.DataAccess/PaginatedListAsync.cs
+++ src/BeepBong.DataAccess/PaginatedListAsync.cs
@@ -13,7 +13,7 @@ namespace BeepBong.DataAccess
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; } 
 
-        public PaginatedList(List<T> items, int count, int pageSize, int pageIndex)
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
@@ -30,7 +30,7 @@ namespace BeepBong.DataAccess
         {
             get => (PageIndex < TotalPages);
         }
-        
+
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             var count = await ((IQueryable<T>)source).CountAsync();
