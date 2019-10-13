8f0ecb2eadc6038c58e21dcdd8274000de5cb8a8
Jimmyson <jimmyson.2007@gmail.com>
2019-10-13 14:19:24 +1100

    Move around components and add programme screen

--- src/BeepBong.Web.Vue/ClientApp/boot.ts
+++ src/BeepBong.Web.Vue/ClientApp/boot.ts
@@ -5,13 +5,14 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: require('./components/home/home.vue.html').default },
-    { path: '/counter', component: require('./components/counter/counter.vue.html').default },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html').default }
+    { path: '/', component: require('./views/home/home.vue.html').default },
+    { path: '/counter', component: require('./views/counter/counter.vue.html').default },
+    { path: '/fetchdata', component: require('./views/fetchdata/fetchdata.vue.html').default },
+    { path: '/programme/:id', component: require('./views/programme/programme.vue.html').default}
 ];
 
 new Vue({
     el: '#app-root',
     router: new VueRouter({ mode: 'history', routes: routes }),
-    render: h => h(require('./components/app/app.vue.html').default)
+    render: h => h(require('./views/app/app.vue.html').default)
 });
