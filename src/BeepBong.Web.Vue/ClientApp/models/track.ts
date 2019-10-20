095856eaa9380d711adda4027e5a4d7e677b3043
Jimmyson <jimmyson.2007@gmail.com>
2019-10-20 21:09:37 +1100

    Dynamic Track Detail screen (#14)

---
+++ src/BeepBong.Web.Vue/ClientApp/models/track.ts
@@ -0,0 +1,13 @@
+export class Track {
+    constructor() {
+        this.trackId = "";
+        this.name = "";
+        this.description = "";
+        this.note = "";
+    }
+
+    trackId: string;
+    name: string;
+    description: string;
+    note: string;
+}
