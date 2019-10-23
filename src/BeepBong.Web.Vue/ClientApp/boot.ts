520921559cc00116910f0aa16e0326978c3a7b0d
Jimmyson <jimmyson.2007@gmail.com>
2019-10-23 11:35:41 +1100

    Routing Param/Query standardisation (#14)

--- src/BeepBong.Web.Vue/ClientApp/boot.ts
+++ src/BeepBong.Web.Vue/ClientApp/boot.ts
@@ -9,8 +9,8 @@ const routes = [
     { path: '/', component: require('./views/home/home.vue.html').default },
     
     { path: '/broadcaster', component: require('./views/broadcaster/list/list.vue.html').default }, // List of broadcaster
-    { path: '/broadcaster/editor', component: require('./views/broadcaster/editor/editor.vue.html').default }, // Broadcaster Editor
-    { path: '/broadcaster/:id', component: require('./views/broadcaster/detail/detail.vue.html').default }, // Broadcaster and Channel List
+    { path: '/broadcaster/editor', name: 'broadcaster-editor', component: require('./views/broadcaster/editor/editor.vue.html').default }, // Broadcaster Editor
+    { path: '/broadcaster/:id', name: 'broadcaster-detail', component: require('./views/broadcaster/detail/detail.vue.html').default }, // Broadcaster and Channel List
     
     { path: '/channel', component: require('./views/channel/list/list.vue.html').default }, // List of Channels
     { path: '/channel/editor', name: 'channel-editor', component: require('./views/channel/editor/editor.vue.html').default }, // Channel Editor
@@ -21,7 +21,7 @@ const routes = [
     { path: '/programme/:id', name: 'programme-detail', component: require('./views/programme/detail/detail.vue.html').default }, // Programme and Tracklists
 
     { path: '/tracklist', component: require('./views/tracklist/list/list.vue.html').default }, // List of Tracklists
-    { path: '/tracklist/editor', component: require('./views/tracklist/editor/editor.vue.html').default }, // Tacklist Editor
+    { path: '/tracklist/editor', name: 'tracklist-editor', component: require('./views/tracklist/editor/editor.vue.html').default }, // Tacklist Editor
     { path: '/tracklist/:id', name: 'tracklist-detail', component: require('./views/tracklist/detail/detail.vue.html').default }, // Tracklist and Tracks
 
     { path: '/library', component: require('./views/library/list/list.vue.html').default },
@@ -29,7 +29,7 @@ const routes = [
 
     { path: '/track/:id', name: 'track-detail', component: require('./views/track/detail/detail.vue.html').default }, // Track Detail
 
-    { path: '/sample/upload', component: require('./views/sample/upload/upload.vue.html').default }, // Sample Upload
+    { path: '/sample/upload', name: 'sample-editor', component: require('./views/sample/upload/upload.vue.html').default }, // Sample Upload
     { path: '/sample/:id', component: require('./views/sample/detail/detail.vue.html').default }, // Sample Detail
     //{ path: '/sample/:id/edit', component: require('./views/sample/edit/edit.vue.html').default }, // Sample Edit
 
